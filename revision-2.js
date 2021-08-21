b0a2b00b53986082d1b4620e4e142af817a67c35
marcdahan <marcdahan@gmail.com>
2021-08-21 19:26:09 +0200

    blabla

---
+++ revision-2.js
@@ -0,0 +1,21 @@
+var nom0 = "tata"
+
+console.log('avant le bloc ' + nom0)
+
+if (true) {
+    var nom = "titi"
+    console.log('dansle bloc ' + nom0)
+
+}
+console.log('après le bloc' + nom0)
+
+let nom1 = "lulu"
+
+console.log('avant le bloc ' + nom1)
+
+if (true) {
+    let nom1 = "riri"
+    console.log('dansle bloc ' + nom1)
+
+}
+console.log('après le bloc ' + nom1)
